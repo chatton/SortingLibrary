1caf9d41208a8cfcc69a2f510dbd2d9748fb639a
chatton <cianhatton@gmail.com>
2018-04-26 23:12:58 +0100

    added default implementation to interface and added bubble sort

--- src/com/sorting/Sorter.java
+++ src/com/sorting/Sorter.java
@@ -1,11 +1,16 @@
 package com.sorting;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public interface Sorter<T extends Comparable> {
+public interface Sorter<T extends Comparable<T>> {
 
     void sort(List<T> list);
 
-    Collection<T> sorted(List<T> list);
+    default Collection<T> sorted(List<T> list){
+        List<T> other = new ArrayList<>(list);
+        this.sort(other);
+        return other;
+    }
 }
